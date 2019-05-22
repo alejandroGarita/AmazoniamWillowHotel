a97e6c52a05e62e959cdf2453b9f841214fac351
fonsoBv <31110577+fonsoBv@users.noreply.github.com>
2019-05-21 23:10:19 -0600

    fonso cambio de facilidad

--- AmazoniamWillowHotel/AmazoniamWillowHotel/Controllers/HomeController.cs
+++ AmazoniamWillowHotel/AmazoniamWillowHotel/Controllers/HomeController.cs
@@ -42,23 +42,7 @@ namespace AmazoniamWillowHotel.Controllers
             return View();
         }
 
-        public ActionResult Facilities()
-        {
-            using (var mo = new Models.Hotel_Amazonian_WillowEntities()) {
-                ViewData["Facilities"] = mo.Pagina.Where(x => x.nombre == "Facilidades").Include(p => p.Info).Include(p => p.Info.Select(x => x.Imagen1)).ToList();
-            }
-            return View();
-        }
-
-        public ActionResult InsertFacilitie()
-        {
-            using (var mo = new Models.Hotel_Amazonian_WillowEntities())
-            {
-                ViewData["Status"] = new SelectList(mo.Estado.ToList(), "Id_Estado", "Nombre"); 
-            }
-            //ViewData["Status"] = new SelectList(model.getStatus(), "Id_Estado", "Nombre"); ;
-            return View();
-        }
+      
 
         public ActionResult HowToGet()
         {
