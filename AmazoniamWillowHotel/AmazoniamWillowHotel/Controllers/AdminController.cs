20f48882fa9fe121692be19e1621fa45beffc9f4
jaguero@outcoding.com <jaguero@outcoding.com>
2019-05-29 08:16:47 -0600

    Tabla de habitaciones para cada tipo de habitación

--- AmazoniamWillowHotel/AmazoniamWillowHotel/Controllers/AdminController.cs
+++ AmazoniamWillowHotel/AmazoniamWillowHotel/Controllers/AdminController.cs
@@ -109,9 +109,13 @@ namespace AmazoniamWillowHotel.Controllers
 
         public ActionResult ManageRooms()
         {
-            using (var mo = new Models.Hotel_Amazonian_WillowEntities())
+            if (isNotLogin())
             {
-                ViewData["AdministrarHabitaciones"] = mo.Tipo_Habitacion.Include(p => p.Habitacion).ToList();
+                using (var mo = new Models.Hotel_Amazonian_WillowEntities())
+                {
+                    ViewData["AdministrarHabitaciones"] = mo.Tipo_Habitacion.Include(p => p.Habitacion).ToList();
+                }
+                return View();
             }
             return View();
         }//ManageRooms
